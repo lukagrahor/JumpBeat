1cdeab23ecb4b79cd6c4cf27a3c4cf6b6a6739f1
lukagrahor <41000453+lukagrahor@users.noreply.github.com>
2023-12-15 18:50:42 +0100

    Koda

---
+++ desktop/src/com/lukagrahor/jumpbeat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lukagrahor.jumpbeat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lukagrahor.jumpbeat.JumpBeatClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JumpBeatClass(), config);
+	}
+}
